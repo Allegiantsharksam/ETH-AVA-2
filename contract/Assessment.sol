33650c07ebd16a147e7afcd3387c33d46a42f4a6
Sparsh Shandil <156890127+Allegiantsharksam@users.noreply.github.com>
2024-06-24 17:48:11 +0530

    Update Assessment.sol

--- contract/Assessment.sol
+++ contract/Assessment.sol
@@ -1,6 +1,8 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.8;
 
+//import "hardhat/console.sol";
+
 contract Assessment {
     address payable public owner;
     uint256 public balance;
@@ -8,32 +10,37 @@ contract Assessment {
     event Deposit(uint256 amount);
     event Withdraw(uint256 amount);
 
-    constructor(uint256 initBalance) payable {
+    constructor(uint initBalance) payable {
         owner = payable(msg.sender);
         balance = initBalance;
     }
 
-    function getBalance() public view returns(uint256) {
+    function getBalance() public view returns(uint256){
         return balance;
     }
 
     function deposit(uint256 _amount) public payable {
-        uint256 _previousBalance = balance;
+        uint _previousBalance = balance;
 
+        // make sure this is the owner
         require(msg.sender == owner, "You are not the owner of this account");
 
+        // perform transaction
         balance += _amount;
 
+        // assert transaction completed successfully
         assert(balance == _previousBalance + _amount);
 
+        // emit the event
         emit Deposit(_amount);
     }
 
+    // custom error
     error InsufficientBalance(uint256 balance, uint256 withdrawAmount);
 
     function withdraw(uint256 _withdrawAmount) public {
         require(msg.sender == owner, "You are not the owner of this account");
-        uint256 _previousBalance = balance;
+        uint _previousBalance = balance;
         if (balance < _withdrawAmount) {
             revert InsufficientBalance({
                 balance: balance,
@@ -41,10 +48,13 @@ contract Assessment {
             });
         }
 
+        // withdraw the given amount
         balance -= _withdrawAmount;
 
+        // assert the balance is correct
         assert(balance == (_previousBalance - _withdrawAmount));
 
+        // emit the event
         emit Withdraw(_withdrawAmount);
     }
 }
