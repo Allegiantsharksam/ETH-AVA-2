77857fa396e03210fcacd5bcf2486a8a041c5ec3
Sparsh Shandil <156890127+Allegiantsharksam@users.noreply.github.com>
2024-06-24 16:00:23 +0530

    Update deploy.js

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,6 +1,6 @@
 import { useState, useEffect } from "react";
 import { ethers } from "ethers";
-import atm_abi from "../artifacts/contract.sol/contract.json";
+import atm_abi from  "../artifacts/contract/Assessment.sol/Assessment.json";
 
 export default function HomePage() {
   const [ethWallet, setEthWallet] = useState(undefined);
@@ -8,7 +8,7 @@ export default function HomePage() {
   const [atm, setATM] = useState(undefined);
   const [balance, setBalance] = useState(undefined);
 
-  const contractAddress = "0x5FbDB2315678afecb367f032d93F642f64180aa3";
+  const contractAddress = "0x5FbDB2315678afecb367f032d93F642f64180aa3"; ////
   const atmABI = atm_abi.abi;
 
   useEffect(() => {
