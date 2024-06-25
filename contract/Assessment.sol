bac4c9b52f8383b0e9620cdbbd27355efbec88ad
Sparsh Shandil <156890127+Allegiantsharksam@users.noreply.github.com>
2024-06-25 20:10:37 +0530

    Update Assessment.sol

--- contract/Assessment.sol
+++ contract/Assessment.sol
@@ -1,54 +1,52 @@
-// SPDX-License-Identifier: MIT
-pragma solidity ^0.8.8;
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.9;
+
+//import "hardhat/console.sol";
 
 contract Assessment {
     address payable public owner;
     uint256 public balance;
 
-    event Deposit(address indexed depositor, uint256 amount);
-    event Withdraw(address indexed recipient, uint256 amount);
+    event Deposit(uint256 amount);
+    event Withdraw(uint256 amount);
 
-    modifier onlyOwner() {
-        require(msg.sender == owner, "Only the owner can call this function");
-        _;
-    }
-
-    constructor(uint256 initBalance) payable {
+    constructor(uint initBalance) payable {
         owner = payable(msg.sender);
         balance = initBalance;
     }
 
-    function getBalance() public view returns (uint256) {
+    function getBalance() public view returns(uint256){
         return balance;
     }
 
-    function deposit(uint256 _amount) public payable onlyOwner {
+    function deposit(uint256 _amount) public payable {
+        uint _previousBalance = balance;
+
+        require(msg.sender == owner, "You are not the owner.");
+
         balance += _amount;
-        emit Deposit(msg.sender, _amount);
+
+        assert(balance == _previousBalance + _amount);
+
+        emit Deposit(_amount);
     }
 
-    function withdraw(uint256 _withdrawAmount) public onlyOwner {
-        require(balance >= _withdrawAmount, "Insufficient balance");
+    error InsufficientBalance(uint256 balance, uint256 withdrawAmount);
+
+    function withdraw(uint256 _withdrawAmount) public {
+        require(msg.sender == owner, "You are not the owner.");
+        uint _previousBalance = balance;
+        if (balance < _withdrawAmount) {
+            revert InsufficientBalance({
+                balance: balance,
+                withdrawAmount: _withdrawAmount
+            });
+        }
 
         balance -= _withdrawAmount;
-        emit Withdraw(msg.sender, _withdrawAmount);
-    }
 
-    // Allow the owner to transfer ownership to a new address
-    function transferOwnership(address payable _newOwner) public onlyOwner {
-        require(_newOwner != address(0), "Invalid new owner address");
-        owner = _newOwner;
-    }
+        assert(balance == (_previousBalance - _withdrawAmount));
 
-    // Fallback function to receive Ether
-    receive() external payable {
-        balance += msg.value;
-        emit Deposit(msg.sender, msg.value);
-    }
-
-    // Withdraw Ether from the contract (onlyOwner)
-    function withdrawEther(uint256 _amount) public onlyOwner {
-        require(_amount <= address(this).balance, "Insufficient contract balance");
-        owner.transfer(_amount);
+        emit Withdraw(_withdrawAmount);
     }
 }
